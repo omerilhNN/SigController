cd1bca9db7a24a9bb29f795e071e05882cc7f9f4
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2024-07-31 17:03:57 +0300

    last

--- Controller.cs
+++ Controller.cs
@@ -4,12 +4,10 @@ using System.Runtime.InteropServices;
 
 class Controller
 {
-    [DllImport("kernel32.dll", SetLastError = true)]
-    static extern bool GenerateConsoleCtrlEvent(uint dwCtrlEvent, uint dwProcessGroupId);
-
     const uint CTRL_C_EVENT = 0;
     const uint CTRL_BREAK_EVENT = 1;
-
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern bool GenerateConsoleCtrlEvent(uint dwCtrlEvent, uint dwProcessGroupId);
     [DllImport("kernel32.dll", SetLastError = true)]
     static extern bool AttachConsole(uint dwProcessId);
 
@@ -19,36 +17,43 @@ class Controller
     [DllImport("kernel32.dll")]
     static extern uint GetLastError();
 
-    const uint ATTACH_PARENT_PROCESS = 0xFFFFFFFF;
 
     static void Main(string[] args)
     {
         string processName = "Recv"; // Aranacak process'in ismi
-        int firstProcessId;
 
         Process[] processes = Process.GetProcessesByName(processName);
         if (processes.Length > 0)
         {
             // İlk eşleşen process'in PID'sini al ve yazdır
-            firstProcessId = processes[0].Id;
+            int firstProcessId = processes[0].Id;
             Console.WriteLine($"First process with name '{processName}' has PID: {firstProcessId}");
 
             Console.WriteLine("Press Ctrl+Break to send the signal...");
 
             Console.CancelKeyPress += (sender, e) =>
             {
-                bool result = GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, (uint)firstProcessId);
+                e.Cancel = true; // Uygulamayı hemen sonlandırmayı önler
 
-                if (result)
+                // Konsolu serbest bırak ve hedef process'in konsoluna bağlan - Bir Process aynı anda 1 tek konsola bağlı olabilir.
+                FreeConsole();
+                if (AttachConsole((uint)firstProcessId))
                 {
-                    Console.WriteLine("CTRL+BREAK signal sent successfully.");
+                    bool result = GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, 0);
+
+                    if (result)
+                    {
+                        Console.WriteLine("CTRL+BREAK signal sent successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to send CTRL+BREAK signal. Error code: {Marshal.GetLastWin32Error()}");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"Failed to send CTRL+BREAK signal. Error code: {GetLastError()}");
+                    Console.WriteLine($"Failed to attach to console of process {firstProcessId}. Error code: {Marshal.GetLastWin32Error()}");
                 }
-
-                processes[0].WaitForExit();
             };
 
             // Sonsuz döngü, kullanıcı girişini bekler
