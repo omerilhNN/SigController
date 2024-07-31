6380a8ecb8eee2bd0a59728a29480dc21d8b0e08
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2024-07-31 09:01:29 +0300

    namedpipe

--- Controller.cs
+++ Controller.cs
@@ -1,66 +1,40 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
+using System.IO;
+using System.IO.Pipes;
 
 class Controller
 {
-    [DllImport("kernel32.dll", SetLastError = true)]
-    static extern bool GenerateConsoleCtrlEvent(uint dwCtrlEvent, uint dwProcessGroupId);
-
-    private const uint CTRL_BREAK_EVENT = 1;
-    private static Process targetProcess;
+    static Process receiverProcess;
 
     static void Main(string[] args)
     {
-        string targetProcessName = "Process"; // Receiver process adını buraya yazın
-        uint targetProcessId = GetProcessIdByName(targetProcessName);
+        string pipeName = "testpipe";
 
-        if (targetProcessId == 0)
+        using (NamedPipeServerStream pipeServer = new NamedPipeServerStream(pipeName))
         {
-            Console.WriteLine($"Process {targetProcessName} not found.");
-            return;
+            Console.WriteLine("Named Pipe Server created. Waiting for connection...");
+
+            pipeServer.WaitForConnection();
+
+            Console.WriteLine("Receiver process connected. Press Ctrl+C to send Control Break signal...");
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                using (StreamWriter writer = new StreamWriter(pipeServer))
+                {
+                    writer.AutoFlush = true;
+                    writer.WriteLine("ControlBreak");
+                }
+                receiverProcess.WaitForExit();
+                Environment.Exit(0);
+            };
+
+            // Infinite loop to keep the controller running
+            while (true)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
         }
-
-        Console.WriteLine($"Target process ID: {targetProcessId}");
-        targetProcess = Process.GetProcessById((int)targetProcessId);
-
-        Console.CancelKeyPress += new ConsoleCancelEventHandler(OnCancelKeyPress);
-
-        Console.WriteLine("Press CTRL+C to send Control + Break signal to the Receiver process.");
-        while (true)
-        {
-            System.Threading.Thread.Sleep(1000);
-        }
-    }
-
-    private static void OnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
-    {
-        e.Cancel = true; // Uygulamanın kapanmasını engelle
-        SendCtrlBreakSignal();
-    }
-
-    private static void SendCtrlBreakSignal()
-    {
-        // Control + Break sinyalini gönder
-        if (GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, (uint)targetProcess.Id))
-        {
-            Console.WriteLine("Control + Break signal sent successfully.");
-        }
-        else
-        {
-            Console.WriteLine("Failed to send Control + Break signal. Error: " + Marshal.GetLastWin32Error());
-        }
-    }
-
-    private static uint GetProcessIdByName(string processName)
-    {
-        Process[] processes = Process.GetProcessesByName(processName);
-        if (processes.Length == 0)
-        {
-            return 0; // Process bulunamadı
-        }
-
-        // İlk eşleşen işlemi döndür (birden fazla aynı isimli işlem olabilir)
-        return (uint)processes[0].Id;
     }
 }
